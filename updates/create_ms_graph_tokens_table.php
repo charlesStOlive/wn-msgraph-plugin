fdb46505064ad8f266a7ddb0ae09457e2cba7186
Charles <charles@notilac.fr>
2022-04-05 11:28:56 +0200

    correction update

--- updates/create_ms_graph_tokens_table.php
+++ updates/create_ms_graph_tokens_table.php
@@ -4,7 +4,7 @@ use Winter\Storm\Database\Schema\Blueprint;
 use Winter\Storm\Database\Updates\Migration;
 use Schema;
 
-class CreateTaskTypesTable extends Migration
+class CreateMsGraphTokenTable extends Migration
 {
     public function up()
     {
